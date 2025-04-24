9fe923789920a86a09e71ad4214445f1263bdcfe
emosweaty <ymkeschoofs04@gmail.com>
2025-04-24 17:49:55 +0200

    Back button

--- flutter_project/lib/login/form.dart
+++ flutter_project/lib/login/form.dart
@@ -28,104 +28,125 @@ class AuthForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(maxWidth: 400),
-      child: 
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: const [BoxShadow(
-              color: Colors.black,
-              blurRadius: 5,
-              offset: Offset(0, 2)
-            )]
-          ),
-          padding: const EdgeInsets.fromLTRB(55, 25, 55, 25),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: const [BoxShadow(
+                color: Colors.black,
+                blurRadius: 5,
+                offset: Offset(0, 2)
+              )]
+            ),
+            padding: const EdgeInsets.fromLTRB(55, 25, 55, 25),
 
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                btnLabel,
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-              
-              SizedBox(height: 30),
-
-
-              if (error != null)...[
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
                 Text(
-                  error!,
-                  style: TextStyle(color: Colors.redAccent)
+                  'BorrowBuddy',
+                  style: TextStyle(
+                    fontFamily: 'Dongle',
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 116, 220, 238)
+                  ),
                 ),
 
-                SizedBox(height: 10)
-              ],
-
-              TextField(
-                controller: email,
-                decoration: InputDecoration(
-                  hintText: 'email',
-                  border: OutlineInputBorder(),
-                  hintStyle: TextStyle(
-                    fontSize: 15,
-                    color: const Color.fromARGB(255, 189, 189, 189)
+                Text(
+                  btnLabel,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
                   ),
                   ),
-              ),
+                
+                SizedBox(height: 20),
 
-              SizedBox(height: 10),
 
-              TextField(
-                controller: passwd,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'password',
-                  border: OutlineInputBorder(),
-                  hintStyle: TextStyle(
-                    fontSize: 15,
-                    color: const Color.fromARGB(255, 189, 189, 189)
+                if (error != null)...[
+                  Text(
+                    error!,
+                    style: TextStyle(color: Colors.redAccent)
                   ),
-                  ),
-              ),
 
-              SizedBox(height: 40),
-
-              isLoading ? const CircularProgressIndicator()
-                : ElevatedButton(
-                  onPressed: onSubmit,
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                    backgroundColor: Colors.blueAccent,
-                    foregroundColor: Colors.white
-                  ),
-                  child: Text(btnLabel)
-              ),
-
-              SizedBox(height: 10),
-              
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(label),
-                  TextButton(
-                    onPressed: () => Navigator.pushReplacementNamed(context, routeName), 
-                    child: Text(
-                      hreflabel,
-                      style: TextStyle(
-                        decoration: TextDecoration.underline,
-                        color: Colors.blueAccent,
-                        decorationColor: Colors.blueAccent,
-                      ),
-                      )
-                  )
+                  SizedBox(height: 10)
                 ],
-              )
-            ],
+
+                TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    hintText: 'email',
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: const Color.fromARGB(255, 189, 189, 189)
+                    ),
+                    ),
+                ),
+
+                SizedBox(height: 10),
+
+                TextField(
+                  controller: passwd,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'password',
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: const Color.fromARGB(255, 189, 189, 189)
+                    ),
+                    ),
+                ),
+
+                SizedBox(height: 40),
+
+                isLoading ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                    onPressed: onSubmit,
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      backgroundColor: Colors.blueAccent,
+                      foregroundColor: Colors.white
+                    ),
+                    child: Text(btnLabel)
+                ),
+
+                SizedBox(height: 10),
+                
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(label),
+                    TextButton(
+                      onPressed: () => Navigator.pushReplacementNamed(context, routeName), 
+                      child: Text(
+                        hreflabel,
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blueAccent,
+                          decorationColor: Colors.blueAccent,
+                        ),
+                        )
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
-        )
+          Positioned(
+            top: 35,
+            left: 20,
+            child:  IconButton(
+            onPressed: ()=> Navigator.pop(context), 
+            icon: Icon(Icons.arrow_back)
+            )
+          )
+        ],
+      )
     );
   }
 }
